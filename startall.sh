794edbb531d51f6f36acf2fafd8684fbafeab0d1
Joseph Dailey <celowsky@wisc.edu>
2016-07-15 11:08:10 -0500

    fixed references to name of repo/folder

--- startall.sh
+++ startall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/nohup /usr/bin/rethinkdb --directory /home/admin/soda-machine/rethinkdb1 --http-port 5000 --bind all &
-/usr/bin/nohup /home/admin/soda-machine/bin/sodaserver &
-/usr/bin/nohup /home/admin/soda-machine/bin/www &
-/usr/bin/nohup /usr/bin/chateau -f /home/admin/soda-machine/config.js -p 5002 &
+/usr/bin/nohup /usr/bin/rethinkdb --directory /home/admin/khk-soda-machine/rethinkdb1 --http-port 5000 --bind all &
+/usr/bin/nohup /home/admin/khk-soda-machine/bin/sodaserver &
+/usr/bin/nohup /home/admin/khk-soda-machine/bin/www &
+/usr/bin/nohup /usr/bin/chateau -f /home/admin/khk-soda-machine/config.js -p 5002 &
